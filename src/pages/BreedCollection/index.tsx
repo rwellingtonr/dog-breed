5ca26e416f912d0be302ef3e8bc3cf588ae06148
Wellington Leardini <wellington-158@hotmail.com>
2022-08-13 10:25:08 -0300

    criada as Pages

---
+++ src/pages/BreedCollection/index.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function BreedCollection() {
+	return <div>BreadCollection</div>
+}
---
+++ src/pages/BreedsNav/index.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function BreedsNav() {
+	return <div>BreedsNav</div>
+}
