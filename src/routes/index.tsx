d3a9e45e0b75a4a2faaaef08032195d49a787cc2
Wellington Leardini <wellington-158@hotmail.com>
2022-08-13 10:26:00 -0300

    definida as rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,26 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom"
+import RequireAuth from "../components/validation"
+import BreedCollection from "../pages/BreedCollection"
+import BreedsNav from "../pages/BreedsNav"
+import Home from "../pages/Home"
 
 export default function MainRoutes() {
 	return (
 		<Routes>
-			<Route path="/"></Route>
+			<Route path="/" element={<Home />}>
+				<Route
+					path="breeds"
+					element={
+						<RequireAuth>
+							<BreedsNav />
+						</RequireAuth>
+					}
+				>
+					<Route path=":breed" element={<BreedCollection />} />
+					<Route path="*" element={<h1>Error 404</h1>} />
+				</Route>
+			</Route>
 		</Routes>
 	)
 }
